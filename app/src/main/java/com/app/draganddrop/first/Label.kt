2605794514c6408cc6cc373a6a5e68ca937eedda
Rohit Kumar <rohitkumar377024@gmail.com>
2019-07-12 12:49:27 +0530

    Initial Commit All comments exist for now.

---
+++ app/src/main/java/com/app/draganddrop/first/Label.kt
@@ -0,0 +1,271 @@
+package com.app.draganddrop.first
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.core.content.ContextCompat
+import com.app.draganddrop.R
+import androidx.core.content.res.ResourcesCompat
+
+class Label : RelativeLayout/* , View.OnTouchListener */ {
+
+//    var width2 = 0//v?.layoutParams?.width
+//    var height2 = 0//v?.layoutParams?.height
+
+    //Text Size Changing SeekBar Values
+    private val step = 1
+    private val min = 24
+    private val max = 72
+
+//    //Both help in getting the drag physics precise
+//    private var dX: Float? = null //DeltaX for detecting change in X
+//    private var dY: Float? = null //DeltaY for detecting change in Y
+//
+//    private var lastAction: Int? = null //Tracks last action for differentiating b/w Touch and Click
+
+    private lateinit var propertiesTitleLL: LinearLayout //Top LL shows 'Properties' and 'X'
+    private lateinit var changeTextSizeSeekBarLL: LinearLayout //LL showing options to change text size
+    private lateinit var changeTextLL: LinearLayout //LL showing options to change text
+    private lateinit var labelBottomLL: LinearLayout //LL showing main buttons for changing text and size
+    private lateinit var fontWeightLL: LinearLayout //LL showing font weights for changing how text looks
+    private lateinit var sample: TextView //Actual TextView
+    private lateinit var changeTextBtn: Button //Main button for changing text
+    private lateinit var changeTextSizeBtn: Button //Main button for changing text size
+    private lateinit var textSizeSeekBar: SeekBar //SeekBar to change the text size
+    private lateinit var propertiesCloseBtn: ImageView //'X' Button that closes the property pane
+    private lateinit var changeTextSizeDoneBtn: TextView //Done button for changing text size
+    private lateinit var changeTextEditText: EditText //EditText that takes value for changing the existing text
+    private lateinit var changeTextDoneBtn: TextView //Done button for changing text
+    private lateinit var fontWeightThinBtn: Button //Sets font weight to Thin
+    private lateinit var fontWeightLightBtn: Button //Sets font weight to Light
+    private lateinit var fontWeightMediumBtn: Button //Sets font weight to Medium
+    private lateinit var fontWeightBoldBtn: Button //Sets font weight to Bold
+
+    //Inflating Label Layout in Constructors
+    constructor(context: Context?) : super(context) {
+        setupView()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        setupView()
+    }
+
+    //Code for Inflation
+    private fun setupView() {
+        inflate(context, R.layout.label_layout, this)
+    }
+
+    //Called once Layout Inflation is over
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        findViewByIds() // --> Connecting IDs with Views
+        configureTextSizeChangeSeekBar() // --> Configures the seekBar for changing text size
+        showPropertiesPane() // --> Hide propertiesPane At Start //todo -- change to hide
+        handlePropertiesCloseBtn() // --> Handles the closing of property pane
+        hide(changeTextSizeSeekBarLL)  // --> Initially textSizeSeekBar gone
+        sample.setOnTouchListener(OnDragTouchListener(this, sample)) // --> adding touch listener here
+        //Handling Change Text Size Stuff
+        changeTextSizeBtn.setOnClickListener { show(changeTextSizeSeekBarLL) } // --> Show textSizeSeekBar
+        changeTextSizeDoneBtn.setOnClickListener { hide(changeTextSizeSeekBarLL) } // --> Hide textSizeSeekBar
+        //Handling Change Text Stuff
+        changeTextBtn.setOnClickListener { handleChangeTextBtn() }
+        changeTextDoneBtn.setOnClickListener { handleChangedText(it) }
+        //Handling Font Weight
+        handleFontWeight()
+    }
+
+    private fun handleFontWeight() {
+
+        fontWeightThinBtn.setOnClickListener {
+            val typeface = ResourcesCompat.getFont(context, R.font.helvetica_neue_thin)
+            sample.typeface = typeface
+
+            fontWeightThinBtn.setTextColor(ContextCompat.getColor(context, R.color.colorPropertiesYellow))
+
+            fontWeightLightBtn.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+            fontWeightMediumBtn.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+            fontWeightBoldBtn.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+        }
+
+        fontWeightLightBtn.setOnClickListener {
+            val typeface = ResourcesCompat.getFont(context, R.font.helvetica_neue_light)
+            sample.typeface = typeface
+
+            fontWeightLightBtn.setTextColor(ContextCompat.getColor(context, R.color.colorPropertiesYellow))
+
+            fontWeightThinBtn.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+            fontWeightMediumBtn.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+            fontWeightBoldBtn.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+        }
+
+        fontWeightMediumBtn.setOnClickListener {
+            val typeface = ResourcesCompat.getFont(context, R.font.helvetica_neue_medium)
+            sample.typeface = typeface
+
+            fontWeightMediumBtn.setTextColor(ContextCompat.getColor(context, R.color.colorPropertiesYellow))
+
+            fontWeightThinBtn.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+            fontWeightLightBtn.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+            fontWeightBoldBtn.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+        }
+
+        fontWeightBoldBtn.setOnClickListener {
+            val typeface = ResourcesCompat.getFont(context, R.font.helvetica_neue_bold)
+            sample.typeface = typeface
+
+            fontWeightBoldBtn.setTextColor(ContextCompat.getColor(context, R.color.colorPropertiesYellow))
+
+            fontWeightThinBtn.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+            fontWeightLightBtn.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+            fontWeightMediumBtn.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+        }
+    }
+
+    //Handles 'X' Button
+    private fun handlePropertiesCloseBtn() {
+        propertiesCloseBtn.setOnClickListener {
+            hidePropertiesPane()  //Hide propertiesPane When 'X' Button Clicked [Close Button]
+            hideSoftKeyboard(it) //Closes Soft Keyboard if user is changing text and suddenly presses 'X'
+        }
+    }
+
+    private fun findViewByIds() {
+        //Initializing here so its not null
+        propertiesTitleLL = findViewById(R.id.label_properties_title_ll)
+        changeTextSizeSeekBarLL = findViewById(R.id.label_seekbar_ll)
+        changeTextLL = findViewById(R.id.label_change_text_ll)
+        labelBottomLL = findViewById(R.id.label_bottom_buttons_ll)
+        fontWeightLL = findViewById(R.id.label_font_weight_ll)
+        changeTextBtn = findViewById(R.id.label_change_text_main_btn)
+        sample = findViewById(R.id.label_sample_textview)
+        textSizeSeekBar = findViewById(R.id.label_text_size_seekbar)
+        changeTextSizeBtn = findViewById(R.id.label_change_text_size_main_btn)
+        changeTextSizeDoneBtn = findViewById(R.id.label_text_size_done_btn)
+        propertiesCloseBtn = findViewById(R.id.label_properties_close_btn)
+        changeTextEditText = findViewById(R.id.label_text_change_edittext)
+        changeTextDoneBtn = findViewById(R.id.label_text_change_done_btn)
+        fontWeightThinBtn = findViewById(R.id.label_font_weight_thin)
+        fontWeightLightBtn = findViewById(R.id.label_font_weight_light)
+        fontWeightMediumBtn = findViewById(R.id.label_font_weight_medium)
+        fontWeightBoldBtn = findViewById(R.id.label_font_weight_bold)
+    }
+
+    //Hide the properties pane -> All LL
+    private fun hidePropertiesPane() {
+        hide(changeTextSizeSeekBarLL)
+        hide(changeTextLL); hide(propertiesTitleLL); hide(labelBottomLL); hide(fontWeightLL)
+    }
+
+    //Show the properties pane - All LL except SeekBar and ChangeText one
+    fun showPropertiesPane() {
+        show(propertiesTitleLL); show(labelBottomLL); show(fontWeightLL)
+    }
+
+    private fun hide(view: View) {
+        view.visibility = View.GONE
+    } // --> Hides a view
+
+    private fun show(view: View) {
+        view.visibility = View.VISIBLE
+    } // --> Shows a view
+
+    //Hides the soft keyboard
+    private fun hideSoftKeyboard(view: View) {
+        val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    //Shows the soft keyboard
+    private fun showSoftKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+    }
+
+    //Shows change text LL, request focus for editText and shows soft keyboard
+    private fun handleChangeTextBtn() {
+        show(changeTextLL)
+        changeTextEditText.requestFocus()
+        showSoftKeyboard()
+    }
+
+    //Handles changing of text
+    private fun handleChangedText(view: View) {
+        hide(changeTextLL)
+        val existingText = sample.text
+        val changedText = changeTextEditText.text.toString()
+        //Handles Sample Text Change
+        sample.text = when {
+            changedText.isEmpty() -> existingText
+            else -> changedText
+        }
+        hideSoftKeyboard(view)
+    }
+
+    //Handles the min, max and step for seekBar changing text size
+    private fun configureTextSizeChangeSeekBar() {
+        textSizeSeekBar.max = (max - min) / step
+        textSizeSeekBar.progress =
+            (max - min) / 2 // --> Initial Progress [24 min 72 max ->> 48 mid]
+        textSizeSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val value = min + progress * step
+                sample.textSize = value.toFloat()
+            }
+        })
+    }
+
+//    override fun onSizeChanged(xNew: Int, yNew: Int, xOld: Int, yOld: Int) {
+//        super.onSizeChanged(xNew, yNew, xOld, yOld)
+//        width2 = xNew
+//        height2 = yNew
+//    }
+
+//    //Handling Drag and Drop Operation --> Smooth and Precise
+//    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+//
+//        val width = v?.layoutParams?.width
+//        val height = v?.layoutParams?.height
+//
+//        Log.d("texter->width", "$width")
+//        Log.d("texter->height", "$height")
+//
+//        when (event?.action) {
+//            MotionEvent.ACTION_DOWN -> {
+//                lastAction = MotionEvent.ACTION_DOWN
+//                dX = v?.x?.minus(event.rawX) //actual x - raw x [difference between cursor and actual drag]
+//                dY = v?.y?.minus(event.rawY) //actual x - raw x [difference between cursor and actual drag]
+//            }
+//            MotionEvent.ACTION_MOVE -> {
+//                lastAction = MotionEvent.ACTION_MOVE  //v?.x = event.rawX + dX!! //v?.y = event.rawY + dY!!
+//
+//
+//            }
+//            MotionEvent.ACTION_UP ->
+//                if (lastAction == MotionEvent.ACTION_DOWN) { //Which means it is --> Clicked
+//                    showPropertiesPane() //Show Properties Pane When Clicked
+//                }
+//        }
+//        return true
+//    }
+
+    //Animation stuff -->
+
+//     val titleLLHeight = propertiesTitleLL.height.toFloat()
+//            val labelBottomLLHeight = labelBottomLL.height.toFloat()
+//            val changeTextLLHeight = changeTextLL.height.toFloat()
+//            val seekBarLLHeight = changeTextSizeSeekBarLL.height.toFloat()
+//
+//            changeTextLL.animate()
+//                .translationY(-changeTextLLHeight)
+//                .alpha(1f)
+//                .setListener(null)
+
+}
---
+++ app/src/main/java/com/app/draganddrop/first/OnDragTouchListener.kt
@@ -0,0 +1,245 @@
+package com.app.draganddrop.first
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Rect
+import android.os.Handler
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewConfiguration
+import kotlin.math.abs
+
+class OnDragTouchListener @JvmOverloads constructor(
+    label: Label, view: View, parent: View = view.parent as View,
+    onDragActionListener: OnDragActionListener? = null) : View.OnTouchListener {
+
+    val label2: Label = label
+
+//    private var lastAction: Int? = null //Tracks last action for differentiating b/w Touch and Click
+
+    private var mView: View? = null
+    private var mParent: View? = null
+    private var isDragging: Boolean = false
+    private var isInitialized = false
+
+    private var width: Int = 0
+    private var xWhenAttached: Float = 0.toFloat()
+    private var maxLeft: Float = 0.toFloat()
+    private var maxRight: Float = 0.toFloat()
+    private var dX: Float = 0.toFloat()
+
+    private var height: Int = 0
+    private var yWhenAttached: Float = 0.toFloat()
+    private var maxTop: Float = 0.toFloat()
+    private var maxBottom: Float = 0.toFloat()
+    private var dY: Float = 0.toFloat()
+
+    private var mOnDragActionListener: OnDragActionListener? = null
+
+    private lateinit var mCtx: Context
+
+    private var mDownX: Float = 0.toFloat()
+    private var mDownY: Float = 0.toFloat()
+    private var SCROLL_THRESHOLD = 10f
+    private var isOnClick: Boolean = false
+
+//    private var lastTouchDown: Long = 0
+//    private val CLICK_ACTION_THRESHHOLD = 200
+
+//    val LONG_PRESS_DELAY = 500
+//
+//    val handler = Handler()
+//    var boundaries: Rect? = null
+//
+//    var onTap = Runnable {
+//        handler.postDelayed(onLongPress, LONG_PRESS_DELAY - ViewConfiguration.getTapTimeout().toLong())
+//    }
+//
+//    var onLongPress = Runnable {
+//        // Long Press
+//        Log.d("workx", "you clicked!")
+//        label2.showPropertiesPane() //todo --> weird why not working there
+//    }
+
+    /**
+     * Callback used to indicate when the drag is finished
+     */
+    interface OnDragActionListener {
+        /**
+         * Called when drag event is started
+         *
+         * @param view The view dragged
+         */
+        fun onDragStart(view: View?)
+
+        /**
+         * Called when drag event is completed
+         *
+         * @param view The view dragged
+         */
+        fun onDragEnd(view: View?)
+    }
+
+//    constructor(view: View, onDragActionListener: OnDragActionListener)
+//            : this(view, view.parent as View, onDragActionListener)
+
+    init {
+        initListener(view, parent)
+        setOnDragActionListener(onDragActionListener)
+    }
+
+    private fun setOnDragActionListener(onDragActionListener: OnDragActionListener?) {
+        mOnDragActionListener = onDragActionListener
+    }
+
+    private fun initListener(view: View, parent: View) {
+        mView = view
+        mParent = parent
+        isDragging = false
+        isInitialized = false
+    }
+
+    private fun updateBounds() {
+        updateViewBounds()
+        updateParentBounds()
+        isInitialized = true
+    }
+
+    private fun updateViewBounds() {
+        width = mView!!.width
+        xWhenAttached = mView!!.x
+        dX = 0f
+
+        height = mView!!.height
+        yWhenAttached = mView!!.y
+        dY = 0f
+    }
+
+    private fun updateParentBounds() {
+        maxLeft = 0f
+        maxRight = maxLeft + mParent!!.width
+
+        maxTop = 0f
+        maxBottom = maxTop + mParent!!.height
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        if (isDragging) {
+            val bounds = FloatArray(4)
+            // LEFT
+            bounds[0] = event.rawX + dX
+            if (bounds[0] < maxLeft) {
+                bounds[0] = maxLeft
+            }
+            // RIGHT
+            bounds[2] = bounds[0] + width
+            if (bounds[2] > maxRight) {
+                bounds[2] = maxRight
+                bounds[0] = bounds[2] - width
+            }
+            // TOP
+            bounds[1] = event.rawY + dY
+            if (bounds[1] < maxTop) {
+                bounds[1] = maxTop
+            }
+            // BOTTOM
+            bounds[3] = bounds[1] + height
+            if (bounds[3] > maxBottom) {
+                bounds[3] = maxBottom
+                bounds[1] = bounds[3] - height
+            }
+
+            when (event.action) {
+                MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP -> {
+                    onDragFinish() //existed below together earlier
+
+                    if (isOnClick) {
+                        //TODO onClick code
+                        Log.d("workx", "clicked")
+                        label2.showPropertiesPane() //fix tomorrow
+                    }
+
+//                    handler.removeCallbacks(onLongPress)
+//                    handler.removeCallbacks(onTap)
+                }
+//                MotionEvent.ACTION_UP -> {
+//                    onDragFinish()
+
+//                    handler.removeCallbacks(onLongPress)
+//                    handler.removeCallbacks(onTap)
+
+//                    Log.d("workx", "you clicked!")
+//                    label2.showPropertiesPane() //todo --> weird why not working there
+
+//                    if (System.currentTimeMillis() - lastTouchDown < CLICK_ACTION_THRESHHOLD) {
+//                        Log.d("workx", "you clicked!")
+//                        label2.showPropertiesPane() //todo --> weird why not working there
+//                    }
+
+//                    if (lastAction == MotionEvent.ACTION_DOWN && !isDragging) { //Which means it is --> Clicked
+                        //showPropertiesPane() //Show Properties Pane When Clicked
+                        //Toast.makeText(mCtx, "Lets see", Toast.LENGTH_SHORT).show()
+//                        Log.d("workx", "workx")
+//                        label2.showPropertiesPane() //fix tomorrow
+//                    }
+//                }
+                MotionEvent.ACTION_MOVE -> {
+
+                    if (isOnClick && (abs(mDownX - event.x) > SCROLL_THRESHOLD || Math.abs(mDownY - event.y) > SCROLL_THRESHOLD)) {
+                        Log.i("workx", "movement detected")
+                        isOnClick = false
+                    }
+
+//                    lastAction = MotionEvent.ACTION_MOVE
+
+//                    if (!boundaries!!.contains(v.left + event.x.toInt(), v.top + event.y.toInt())) {
+//                        handler.removeCallbacks(onLongPress)
+//                        handler.removeCallbacks(onTap)
+//                    }
+
+                    mView!!.animate().x(bounds[0]).y(bounds[1]).setDuration(0).start()
+                }
+            }
+            return true
+        } else {
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+//                    lastTouchDown = System.currentTimeMillis();
+//                    lastAction = MotionEvent.ACTION_DOWN
+
+//                    boundaries = Rect(v.left, v.top, v.right, v.bottom)
+//                    handler.postDelayed(onTap, ViewConfiguration.getTapTimeout().toLong())
+
+                    mDownX = event.x
+                    mDownY = event.y
+                    isOnClick = true
+
+                    isDragging = true
+                    if (!isInitialized) {
+                        updateBounds()
+                    }
+                    dX = v.x - event.rawX
+                    dY = v.y - event.rawY
+                    if (mOnDragActionListener != null) {
+                        mOnDragActionListener!!.onDragStart(mView)
+                    }
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private fun onDragFinish() {
+        if (mOnDragActionListener != null) {
+            mOnDragActionListener!!.onDragEnd(mView)
+        }
+
+        dX = 0f
+        dY = 0f
+        isDragging = false
+    }
+
+}
